359ee91a7b2fb7b3fac7503b2c126abc33556a72
Kelvin Yip <kelvinyip7@Kelvins-MacBook-Pro.local>
2022-12-13 15:49:54 -0500

    Begin support for tracking 'deaths' and cleaned up functions

--- model/hand.py
+++ model/hand.py
@@ -3,12 +3,14 @@ import random
 
 class Hands:
     def __init__(self, left_hand, right_hand):
-        """ Creates hands for one player with given sunms
-
+        """ 
+            Creates hands for one player with given sums
             sum -- an integer
         """
         self._left_hand = left_hand
         self._right_hand = right_hand
+        self._left_deaths = 0
+        self._right_deaths = 0
         self._hash = str(self).__hash__()
     
     def left_hand(self):
@@ -40,19 +42,13 @@ class Hands:
         self._right_hand = new_sum
     
     def left_dead(self):
-        if self._left_hand == 0:
-            return True
-        return False
+        return self._left_hand == 0
     
     def right_dead(self):
-        if self._right_hand == 0:
-            return True
-        return False
+        return self._right_hand == 0
 
     def lost(self):
-        if self.left_dead() and self.right_dead():
-            return True
-        return False
+        return self.left_dead() and self.right_dead()
 
     # def __repr__(self):
     #     return "" + str(self._rank) + str(self._suit)
