59cfdc1ecfe5b87dd2a2281fed6a714e0312e968
Kishan Patel <patel.kishan.kbp@gmail.com>
2022-12-19 15:33:56 -0500

    separate tests in test script

--- model/chopsticks/test_chopsticks.py
+++ model/chopsticks/test_chopsticks.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         games = int(sys.argv[1])
 
+    #################################################################################
+    # GREEDY VS RANDOM AGENT HERE
+
     game = Game()
     benchmark = CompositePolicy(game, RandomSplitter(game), RandomAttacker(game), RandomDivider(game))
     submission = CompositePolicy(game, GreedySplitter(game), GreedyAttacker(game), GreedyDivider(game))
@@ -16,3 +19,16 @@ if __name__ == "__main__":
     print(f"P1 won {results[0]} percent of games")
     print(f"P2 won {results[1]} percent of games")
 
+    #################################################################################
+
+    #################################################################################
+    # RULES BASED VS GREEDY AGENT HERE
+    #################################################################################
+
+    #################################################################################
+    # MCTS VS GREEDY AGENT HERE
+    #################################################################################
+
+    #################################################################################
+    # Q-LEARN VS GREEDY AGENT HERE
+    #################################################################################
