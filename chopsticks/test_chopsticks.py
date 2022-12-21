f745b1a03b0e804b1a1c8916a9501b50bfb28efc
Kishan Patel <patel.kishan.kbp@gmail.com>
2022-12-20 22:10:33 -0500

    merge

--- chopsticks/test_chopsticks.py
+++ chopsticks/test_chopsticks.py
@@ -10,6 +10,9 @@ from policy import CompositePolicy, RandomSplitter, RandomAttacker, RandomDivide
 from mcts import MCTS
 from chopsticks import Game, evaluate_policies, evaluate_mcts_policies
 
+# Group Members: Kelvin Yip (khy6), Kishan Patel (kbp27), Megha Joshi (mj573), Andy Wu (aw858)
+# MCTS TAKES A WHILE TO RUN, SHOULD TAKE ROUGHLY 3 MINUTES
+
 if __name__ == "__main__":
     #################################################################################
     # GREEDY VS RANDOM AGENT HERE
@@ -59,7 +62,7 @@ if __name__ == "__main__":
     game = Game()
     benchmark = CompositePolicy(game, GreedySplitter(game), GreedyAttacker(game), GreedyDivider(game))
     submission = MCTS(0.05).mcts_function
-    games = 100
+    games = 1000
 
     results = evaluate_mcts_policies(game, lambda: submission, benchmark, games)
     print("TEST 4: MCTS against a Greedy Agent")
@@ -74,7 +77,7 @@ if __name__ == "__main__":
     benchmark = CompositePolicy(game, RandomSplitter(game), RandomAttacker(game), RandomDivider(game))
     submission = MCTS(0.05).mcts_function
 
-    games = 100
+    games = 1000
     results = evaluate_mcts_policies(game, lambda: submission, benchmark, games)
     print("TEST 5: MCTS against a Random Agent")
     print(f"We are using an MCTS agent to play Chopsticks against a Random agent. Here's our winning ratio over {games} games: {results[0]}")
